f1fdf20071432a2c42b7c7faf6e8e49eafbb1ebd
dayy-one <abdelazizhassani09@gmail.com>
2026-09-08 00:36:22 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -517,3 +517,6 @@ def payout(user):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)), debug=True)
